8868d1301b084720f8bab3fac03f39cb64b56600
Ilja Strobbe <ilja@robonaut.be>
2019-07-24 15:00:44 +0200

    Add typescript declaration file

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+import { EventSubscriptionVendor } from "react-native";
+
+declare module "react-native-sentiance" {
+  interface RNSentianceConstructor extends EventSubscriptionVendor {
+    init(appId: string, secret: string, baseURL: string): Promise<any>;
+    initWithUserLinkingEnabled(
+      appId: string,
+      secret: string,
+      baseURL: string
+    ): Promise<any>;
+    start(): Promise<any>;
+    stop(): Promise<any>;
+    getInitState(): Promise<any>;
+    getSdkStatus(): Promise<any>;
+    getVersion(): Promise<any>;
+    getUserId(): Promise<any>;
+    getUserAccessToken(): Promise<any>;
+    addUserMetadataField(label: string, value: string): Promise<any>;
+    removeUserMetadataField(label: string): Promise<any>;
+    getWiFiQuotaLimit(): Promise<any>;
+    getWiFiQuotaUsage(): Promise<any>;
+    getMobileQuotaLimit(): Promise<any>;
+    getMobileQuotaUsage(): Promise<any>;
+    getDiskQuotaLimit(): Promise<any>;
+    getDiskQuotaUsage(): Promise<any>;
+    disableBatteryOptimization(): Promise<any>;
+    getUserActivity(): Promise<any>;
+    listenUserActivityUpdates(): void;
+  }
+
+  const RNSentiance: RNSentianceConstructor;
+  export default RNSentiance;
+}
