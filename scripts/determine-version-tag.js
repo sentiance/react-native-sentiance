b679c65ca9dc12ddd6da7c99f43aac10f7970375
Prakash Raman <prakash.raman@sentiance.com>
2022-02-21 12:16:46 +0100

    API-2007: Added beta track npm workflow

---
+++ scripts/determine-version-tag.js
@@ -0,0 +1,14 @@
+/**
+ * This script attempts to figure out what the tag for the version could be.
+ * This tag would ideally be used in the "npm publish" of the package.
+ *
+ * 1.1.1 tag = stable 1.1.1-beta.0 = beta etc
+ */
+
+const json = require("../package.json");
+
+const match = json.version.match(/^(\d*\.\d*\.\d*)\-([a-z]*)\.(\d*)$/);
+const tag = (match && match[2]) ?? "stable";
+
+/** Logs the tag for it to be consumed by a different script as an input */
+console.log(tag);
