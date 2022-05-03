09e771806434ceae789461c886dad56f08262dfc
pasan_sum <pasan.sumanathilake@sentiance.com>
2022-05-03 15:37:57 +0530

    debugging attempt 4

--- .sentiance/doc-pr-gen.sh
+++ .sentiance/doc-pr-gen.sh
@@ -2,7 +2,6 @@
 ### Sentiance document pr creation script ###
 
 set -e
-echo "$SENTIANCE_GITHUB_TOKEN"
 rm -rf .docs
 git clone https://sentianceuser:$SENTIANCE_GITHUB_TOKEN@github.com/sentiance/documentation.git .docs
 
