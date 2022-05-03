7fde304b7b4f25b7619d428fa0ee1c7ba25e2ab3
pasan_sum <pasan.sumanathilake@sentiance.com>
2022-05-03 15:52:50 +0530

    debugging attempt 7

--- .sentiance/doc-pr-gen.sh
+++ .sentiance/doc-pr-gen.sh
@@ -3,7 +3,6 @@
 
 set -e
 rm -rf .docs
-echo https://sentianceuser:$SENTIANCE_GITHUB_TOKEN@github.com/sentiance/documentation.git
 git clone https://sentianceuser:$SENTIANCE_GITHUB_TOKEN@github.com/sentiance/documentation.git .docs
 
 # other variables
