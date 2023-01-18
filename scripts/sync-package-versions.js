e00799cb017caaf2af0c615ac70354df797a7b6d
Mohammed Aouf Zouag <mohammed.zouag@sentiance.com>
2023-01-18 14:39:34 +0000

    Update the native iOS modules' versions to match the ones specified on their host JS modules.

--- scripts/sync-package-versions.js
+++ scripts/sync-package-versions.js
@@ -1,36 +1,45 @@
-const SENTIANCE_ORG_NAME = "@sentiance-react-native";
-const SENTIANCE_MODULES = ['core', 'crash-detection', 'user-context', 'legacy'];
-
-let newVersion = process.env.npm_config_newversion;
-let isNewVersionSpecified = true;
-
-if (!newVersion) {
-  console.info('A new version number has not been specified. Falling back to the number indicated on the top level package.json');
-  newVersion = process.env.npm_package_version;
-  isNewVersionSpecified = false;
-}
-
 function updateVersionNumberForTopLevelModule() {
   const saveFile = require('fs').writeFileSync;
   const pkgJsonPath = `${require.main.paths[0].split('node_modules')[0]}../package.json`;
-  const json = require(pkgJsonPath);
-
-  if (json.hasOwnProperty('version')) {
-    json['version'] = newVersion;
-  }
-
+  console.log('Found top level package.json file: ' + pkgJsonPath);
+  const json = updatePackageJsonVersionNumber(pkgJsonPath);
   saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
 }
 
-function updateVersionNumbersForModule(moduleName) {
+function updateVersionNumbersForJsModule(moduleName) {
   const saveFile = require('fs').writeFileSync;
   const pkgJsonPath = `${require.main.paths[0].split('node_modules')[0]}../packages/${moduleName}/package.json`;
-  const json = require(pkgJsonPath);
+  console.log('Found package.json file: ' + pkgJsonPath);
+  let json = updatePackageJsonVersionNumber(pkgJsonPath);
+  json = updatePackageJsonPeerDependenciesVersionNumbers(json);
+  saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
+}
 
+function updateVersionNumberForIosModule(module) {
+  const jsModuleName = module.name;
+  const iosModuleName = module.iosModuleName;
+  const readFile = require('fs').readFile;
+  const saveFile = require('fs').writeFileSync;
+  const modulePodspecPath = `${require.main.paths[0].split('node_modules')[0]}../packages/${jsModuleName}/ios/${iosModuleName}.podspec`;
+  console.log('Found podspec file: ' + modulePodspecPath);
+  readFile(modulePodspecPath, 'utf8', function (error, text) {
+    const match = text.match('(?<prefix>s.version\\s+=\\s+)"(?<versionCode>.+)"');
+    const prefix = match.groups.prefix;
+    const versionCode = match.groups.versionCode;
+    const newText = text.replace(`${prefix}"${versionCode}"`, `${prefix}"${newVersion}"`);
+    saveFile(modulePodspecPath, newText, 'utf8');
+  });
+}
+
+function updatePackageJsonVersionNumber(pkgJsonPath) {
+  const json = require(pkgJsonPath);
   if (json.hasOwnProperty('version')) {
     json['version'] = newVersion;
   }
+  return json;
+}
 
+function updatePackageJsonPeerDependenciesVersionNumbers(json) {
   if (json.hasOwnProperty('peerDependencies')) {
     const peerDeps = Object.entries(json.peerDependencies);
     if (peerDeps.length) {
@@ -48,10 +57,46 @@ function updateVersionNumbersForModule(moduleName) {
     }
   }
 
-  saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
+  return json;
 }
+
+function updateVersionNumbersForModule(module) {
+  updateVersionNumbersForJsModule(module.name);
+  if (module.iosModuleName !== undefined) {
+    updateVersionNumberForIosModule(module);
+  }
+}
+
+const SENTIANCE_ORG_NAME = "@sentiance-react-native";
+let newVersion = process.env.npm_config_newversion;
+let isNewVersionSpecified = true;
+
+if (!newVersion) {
+  console.info('A new version number has not been specified. Falling back to the number indicated on the top level package.json');
+  newVersion = process.env.npm_package_version;
+  console.info('The new version will be: ' + newVersion);
+  isNewVersionSpecified = false;
+}
+
+const TARGET_MODULES = [
+  {
+    name: 'core',
+    iosModuleName: 'RNSentianceCore'
+  },
+  {
+    name: 'crash-detection'
+  },
+  {
+    name: 'user-context'
+  },
+  {
+    name: 'legacy'
+  },
+];
 
 if (isNewVersionSpecified) {
   updateVersionNumberForTopLevelModule();
 }
-SENTIANCE_MODULES.forEach(module => updateVersionNumbersForModule(module));
+
+console.log(`Updating version numbers for ${TARGET_MODULES.length} JS module(s): ${TARGET_MODULES.map(module => module.name).join(', ')}`);
+TARGET_MODULES.forEach(module => updateVersionNumbersForModule(module));
