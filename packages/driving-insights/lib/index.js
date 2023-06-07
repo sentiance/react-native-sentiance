19e82953be0ea34e643683d86e66c24b6dd37a4c
Mohammed Aouf Zouag <mohammed.zouag@sentiance.com>
2023-06-07 06:54:49 +0000

    Expose the getPhoneUsageEvents binding

--- packages/driving-insights/lib/index.js
+++ packages/driving-insights/lib/index.js
@@ -2,10 +2,12 @@ const drivingInsights = require('./driving-insights');
 
 const getDrivingInsights = (transportId: String) => drivingInsights.getDrivingInsights(transportId);
 const getHarshDrivingEvents = (transportId: String) => drivingInsights.getHarshDrivingEvents(transportId);
+const getPhoneUsageEvents = (transportId: String) => drivingInsights.getPhoneUsageEvents(transportId);
 const addDrivingInsightsReadyListener = drivingInsights._addDrivingInsightsReadyListener;
 
 module.exports = {
   getDrivingInsights,
   getHarshDrivingEvents,
+  getPhoneUsageEvents,
   addDrivingInsightsReadyListener
 };
